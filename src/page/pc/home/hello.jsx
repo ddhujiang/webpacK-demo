3e2822799f521ae92fed884296337f48b84901d6
hujiangdd <hujiang10869432@gmail.com>
2019-12-09 11:01:51 +0800

    bug修改

--- src/page/pc/home/hello.jsx
+++ src/page/pc/home/hello.jsx
@@ -3,12 +3,12 @@ import React, {Component} from 'react'; // 这两个模块必须引入
 
 import Loadable from 'react-loadable';
 
-const Win = Loadable({
-    loader: () => import('./../components/win.jsx'),
-    loading: <div/>,
-});
+// const Win = Loadable({
+//     loader: () => import('./../components/win.jsx'),
+//     loading: <div/>,
+// });
 
-// import Win from  './../components/win.jsx'
+import Win from  './../components/win.jsx'
 
 let name = 'All';
 
